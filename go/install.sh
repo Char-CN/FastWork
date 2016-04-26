581a4bccdcb734a0363cbb7a86f7e4a53ebd0405
hyy <hyy@BlazerHe-Mac.local>
2016-04-26 15:39:34 +0800

    update go command

---
+++ go/install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+############################
+# ./install.sh -f          #
+############################
+
+echo "install begin ......."
+s_p="`dirname $0`/bin"
+ipath="/usr/local/bin"
+function _install() {
+    if [[ $3 == "${HOME}" && $2 == ".go" ]]
+    then
+        echo "install ${HOME}/.go exists ... "
+        return
+    fi
+    if [[ $4 == "-f" || ! -f "$3/$2" ]]
+    then
+        echo "install $1/$2 $3"
+        install -v $1/$2 $3
+    else
+        echo "exists $3/$2 "
+    fi
+}
+
+
+_install ${s_p} go ${ipath} $1
+_install ${s_p} goex ${ipath} $1
+_install ${s_p} .go ${ipath} $1
+_install ${s_p} .go ${HOME} $1
+
+
+echo "install done  ......"
