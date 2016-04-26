3ab5ec907934a400ea85481258e69f6dc9c9d651
hyy <hyy@BlazerHe-Mac.local>
2016-04-26 11:43:16 +0800

    asdasdfasdfasdfasdf

---
+++ go/.go
@@ -0,0 +1,14 @@
+#########################################################
+# | paramter :                                          #
+# | alias name                                          #
+# | [-h hostname]                                       #
+# | [-u userName]                                       #
+# | [-P password]                                       #
+# | [-p port]                                           #
+# | usage config :                                      #
+# | hyy -h 192.168.0.110 -u root -P root -p 222         #
+# | hyy -h 192.168.0.110 -u blazer                      #
+#########################################################
+hyy -h 192.168.0.110 -u root -P root -p 222
+hyy -h 192.168.0.110 -u blazer
+
