d10d89d5ac4ea076f6e7740c4fc443db36c44af4
hyy <hyy@BlazerHe-Mac.local>
2016-05-11 10:34:09 +0800

    commit

--- install.sh
+++ install.sh
@@ -6,35 +6,49 @@ s_p="`dirname $0`/bin"
 ipath="/usr/local/bin"
 
 function _install() {
-    if [[ $3 == "${HOME}" && $2 == ".go" ]]
-    then
-        echo "install ${HOME}/.go exists ... "
+    if [[ $2 == ".db" || $2 == ".go" ]]; then
+        echo "install ${HOME}/$2 exists"
         return
     fi
-    if [[ $4 == "-f" || ! -f "$3/$2" ]]
-    then
-        #echo "install $1/$2 $3"
-        install -v $1/$2 $3
+    if [[ $4 == "-f" || ! -f "$3/$2" ]]; then
+        install $1/$2 $3
+        if [ $? != 0 ]; then
+            echo "install $1/$2 $3 [fail]"
+        else
+            echo "install $1/$2 $3 [success]"
+        fi
     else
         echo "exists $3/$2 "
     fi
 }
 
-echo "install begin ......"
+echo "======================================install begin ......"
 
+echo " "
 _install ${s_p} fw_commons ${ipath} $1
 if [ $? != 0 ]; then
     echo "install fail ......"
     exit -1
 fi
 
+echo " "
 _install ${s_p} fw_expect_go ${ipath} $1
 _install ${s_p} fw_expect_goscp ${ipath} $1
+_install ${s_p} fw_expect_db ${ipath} $1
 
+echo " "
 _install ${s_p} go ${ipath} $1
 _install ${s_p} goscp ${ipath} $1
+_install ${s_p} db ${ipath} $1
 
+echo " "
 _install ${s_p} .go ${ipath} $1
 _install ${s_p} .go ${HOME} $1
-echo "install success ......"
+
+echo " "
+_install ${s_p} .db ${ipath} $1
+_install ${s_p} .db ${HOME} $1
+
+echo " "
+echo "======================================install success ......"
 
